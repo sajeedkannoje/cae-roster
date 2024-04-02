3ae66f004d576103180eeafcde2b34e9bf9fa41b
Sajeed <63125230+sajeedkannoje@users.noreply.github.com>
2024-04-02 15:45:50 +0530

    adding the all apis

--- app/Http/Requests/LocationRequest.php
+++ app/Http/Requests/LocationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
 
 class LocationRequest extends FormRequest
 {
@@ -17,7 +18,7 @@ class LocationRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
